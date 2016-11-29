fa0fce2c6f565f839882946706bd13a11d56ba8c
Paulo Corazza <paulovictor@live.com>
2016-11-29 02:17:04 -0200

    Testes com criação de usuário

--- Stack/src/database/MySQLConnect.java
+++ Stack/src/database/MySQLConnect.java
@@ -1,47 +1,43 @@
 package database;
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class MySQLConnect{
-	
-	public void connect(String username,String pwd){
+public class MySQLConnect {
+
+	public void connect(String username, String pwd) {
 		Connection conn = null;
-		try{
-			
+		try {
+
 			String url1 = "jdbc:mysql://sql9.freesqldatabase.com:3306/sql9146901";
-	        String user = "sql9146901";
-	        String password = "tPcthmKbZt";
-	        
-	        conn = DriverManager.getConnection(url1, user, password);
-	        if (conn != null) {
-                System.out.println("Connected to the database FREESQLDATABASE");
-            }
-	        
-			
+			String user = "sql9146901";
+			String password = "tPcthmKbZt";
+
+			conn = DriverManager.getConnection(url1, user, password);
+			if (conn != null) {
+				System.out.println("Connected to the database FREESQLDATABASE");
+			}
 		}
-		
-		catch(SQLException ex){
+
+		catch (SQLException ex) {
 			ex.printStackTrace();
 		}
-		
-		 try {
+
+		try {
 			Statement stmt = (Statement) conn.createStatement();
-			
+
 			String sqlInsert = "insert into user " // need a space
-		               + "VALUES (NULL, '"+username+"', '"+pwd+"', '0', '1')";
-		         System.out.println("The SQL query is: " + sqlInsert);  // Echo for debugging
-		         int countInserted = stmt.executeUpdate(sqlInsert);
-		         System.out.println(countInserted + " records inserted.\n");
+					+ "VALUES (NULL, '" + username + "', '" + pwd + "', '0', '1')";
+			System.out.println("The SQL query is: " + sqlInsert); // Echo for
+																	// debugging
+			int countInserted = stmt.executeUpdate(sqlInsert);
+			System.out.println(countInserted + " records inserted.\n");
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		 
-		
+
 	}
 }
-
--- Stack/src/domain/Main.java
+++ Stack/src/domain/Main.java
@@ -1,14 +1,46 @@
 package domain;
 
+import java.util.Scanner;
+
 import database.MySQLConnect;
+import domain.User.Permission;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		MySQLConnect connect = new MySQLConnect();
-		connect.connect("teste","hostinger");
+		
+		int opcao;
+		Scanner scanner = new Scanner(System.in);		
+		
+		do {
+			System.out.println("1 - Criar novo usuário");
+			System.out.println("2 - Sair");
+			System.out.print("Digite a opção desejada: ");
+			opcao = scanner.nextInt();
+			
+			if (opcao == 1) {
+				
+				String username;
+				String password;
+				
+				System.out.print("Digite o nome do usuário: ");
+				username = scanner.next();
+				System.out.print("Digite a senha: ");
+				password = scanner.next();
+				
+				User user = new User(username, password, Permission.AUTHENTICATED);
+				MySQLConnect connect = new MySQLConnect();
+				connect.connect(user.getUsername(),user.getPassword());
+			}
+		}
+		while (opcao != 2);
+		
+		scanner.close();
+		System.out.println("Saindo...");
+		
+		System.exit(0);
+		
+		
 	}
 
 }
