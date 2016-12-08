815eafe09c044c0c85b160a7aba33f8995b5d852
anderlentz <anderlentz@gmail.com>
2016-12-08 20:21:39 -0200

    versao1 gitignore bug fixed

---
+++ Stack/src/domain/OperationPermission.java
@@ -0,0 +1,61 @@
+package domain;
+
+public class OperationPermission {
+	
+	public static boolean modifyUserPermission(User loggedUser){
+		if (loggedUser.getPermission() == User.Permission.ADMINISTRATOR) {
+			
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+	public static boolean createQuestion(User loggedUser){
+		if(loggedUser.getPermission() != User.Permission.NOT_AUTHENTICATED){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+	public static boolean searchQuestion(User loggedUser){
+		
+		return true;	//todos podem pesquisar uma questão
+	}
+
+	public static boolean displayPostOptions(User loggedUser){
+		
+		if(loggedUser == null){
+			return false;
+		}
+		if(loggedUser.getPermission() != User.Permission.NOT_AUTHENTICATED){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+	public static boolean selectBestAnswer(User loggedUser, Question question){
+		if(loggedUser.getId()==question.getIdAuthor()){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+	public static boolean blockUser(User loggedUser) {
+		if(loggedUser.getPermission() == User.Permission.ADMINISTRATOR 
+				|| loggedUser.getPermission() == User.Permission.MODERATOR){
+			return true;
+			
+		}
+		else{
+			return false;
+		}
+	}
+}
